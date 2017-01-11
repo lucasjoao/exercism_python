a051d8b6a40c7a08054c01f122752aec267a23ac
lucasjoao <lucasjoao.lj@gmail.com>
2017-01-11 21:25:01 -0200

    make rna simple

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,17 +1,10 @@
 def to_rna(text):
   complement = ''
-  char_complement = ''
+  alphabet = {'G' : 'C', 'C' : 'G', 'T' : 'A', 'A' : 'U'}
+
   for char in text:
-    char_complement = transcript(char)
-    if char_complement is '':
+    if char not in alphabet:
       return ''
-    complement += char_complement
+    complement += alphabet[char]
 
   return complement
-
-def transcript(char):
-  alphabet = {'G' : 'C', 'C' : 'G', 'T' : 'A', 'A' : 'U'}
-  if char not in alphabet:
-    return ''
-
-  return alphabet[char]
