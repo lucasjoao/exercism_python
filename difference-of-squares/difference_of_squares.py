889d00dcb38cf858307e600be4eadc92fd2441c2
lucas <lucasjoao.lj@gmail.com>
2017-02-23 19:20:46 -0300

    difference of squares ok

---
+++ difference-of-squares/difference_of_squares.py
@@ -0,0 +1,18 @@
+def sum_of_squares(n):
+    s = 0
+    for i in range(1, n+1):
+        s += i**2
+
+    return s
+
+
+def square_of_sum(n):
+    s = 0
+    for i in range(1, n+1):
+        s += i
+
+    return s**2
+
+
+def difference(n):
+    return square_of_sum(n) - sum_of_squares(n)
