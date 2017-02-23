ba85f9e70a88915988fca4f363109107f9ea60b5
lucas <lucasjoao.lj@gmail.com>
2017-02-23 20:36:17 -0300

    finish anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,16 +1,9 @@
 def detect_anagrams(word, candidates):
     sublist = []
     for w in candidates:
-        if word.lower() == w.lower() or len(word) != len(w):
-            continue
-
-        can_append = True
-        for c in w.lower():
-            if c not in word.lower():
-                can_append = False
-
-
-        if can_append:
+        if word.lower() != w.lower() and \
+        len(word) == len(w) and \
+        sorted(word.lower()) == sorted(w.lower()):
             sublist.append(w)
 
     return sublist
