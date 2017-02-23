ef4c4f19582b9addfe7a30dda4413f3e4dd3b67a
lucas <lucasjoao.lj@gmail.com>
2017-02-22 23:34:25 -0300

    fix to pep8

--- meetup/meetup.py
+++ meetup/meetup.py
@@ -1,7 +1,7 @@
+from datetime import date
 import sys
 sys.path.insert(0, '/home/lucas/Workspace/python/exercism/python/leap')
 
-from datetime import date
 from leap import is_leap_year
 
 day_as_int = {'Monday': 0,
@@ -25,12 +25,8 @@ max_range_by_month = {1: 32,
                       11: 31,
                       12: 32}
 
+
 def meetup_day(year, month, day, pos):
-#     if pos == "teenth":
-        # for i in range(13, 20):
-            # if date(year, month, i).weekday() == day_as_int[day]:
-                # return date(year, month, i)
-#     elif pos
     applicants = []
     m = 30 if is_leap_year(year) else max_range_by_month[month]
     for i in range(1, m):
@@ -53,4 +49,3 @@ def meetup_day(year, month, day, pos):
     for j in applicants:
             if j in range(13, 20):
                 return date(year, month, j)
-
